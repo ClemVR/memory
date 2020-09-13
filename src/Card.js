58ca37173632750a89515e6cf4b27a172203d15c
clemVR <c.vanrech@gmail.com>
2020-09-13 12:06:04 +0200

    Base JSX done

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 import './Card.css'
 
-const HIDDEN_SYMBOL = ''
+const HIDDEN_SYMBOL = '?'
 
-const Card = () => <div className="card" />
+const Card = ({ card,feedback }) => (
+<div className={`card ${feedback}`}>
+    <span className="symbol">
+        {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+    </span>
+</div>
+)
 
 export default Card
