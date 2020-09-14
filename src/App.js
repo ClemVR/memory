739ce2a3a36923abf0eaac22b1ecda8d82dd20f6
clemVR <c.vanrech@gmail.com>
2020-09-14 18:42:43 +0200

    adding HallOfFame

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import GuessCount from './GuessCount'
 import Card from './Card'
 
 class App extends Component {
+  handleCardClick(card){
+    console.log(card,'clicked')
+  }
+
   render() {
+    
     return (
       <div className="memory">
         <GuessCount guesses= {0}/>
-        <Card card ="1" feedback="hidden"/>
-        <Card card ="2" feedback="justMatched"/>
-        <Card card ="3" feedback="justMismatched"/>
-        <Card card ="4" feedback="visible"/>
-        <Card card ="5" feedback="hidden"/>
-        <Card card ="6" feedback="justMatched"/>
+        <Card card ="1" feedback="hidden" onClick={this.handleCardClick}/>
+        <Card card ="2" feedback="justMatched" onClick={this.handleCardClick}/>
+        <Card card ="3" feedback="justMismatched" onClick={this.handleCardClick}/>
+        <Card card ="4" feedback="visible" onClick={this.handleCardClick}/>
+        <Card card ="5" feedback="hidden" onClick={this.handleCardClick}/>
+        <Card card ="6" feedback="justMatched" onClick={this.handleCardClick}/>
+       
       </div>
     )
   }
