3b1592034637805e0818f9d2ed5befd62c00fb04
clemVR <c.vanrech@gmail.com>
2020-09-14 18:45:07 +0200

    adding card creating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
-import GuessCount from './GuessCount'
+
 import Card from './Card'
+import GuessCount from './GuessCount'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component {
-  handleCardClick(card){
-    console.log(card,'clicked')
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
   }
 
   render() {
-    
+    const won = new Date().getSeconds() % 2 === 0
     return (
       <div className="memory">
-        <GuessCount guesses= {0}/>
-        <Card card ="1" feedback="hidden" onClick={this.handleCardClick}/>
-        <Card card ="2" feedback="justMatched" onClick={this.handleCardClick}/>
-        <Card card ="3" feedback="justMismatched" onClick={this.handleCardClick}/>
-        <Card card ="4" feedback="visible" onClick={this.handleCardClick}/>
-        <Card card ="5" feedback="hidden" onClick={this.handleCardClick}/>
-        <Card card ="6" feedback="justMatched" onClick={this.handleCardClick}/>
-       
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card
+          card="💖"
+          feedback="justMismatched"
+          onClick={this.handleCardClick}
+        />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>GAGNÉ !</p>}
       </div>
     )
   }
